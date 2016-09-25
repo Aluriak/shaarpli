cbb58c3392fea2d34d357377320b0ff10b8b88a0
Aluriak <lucas.bourneuf@laposte.net>
2016-09-25 15:40:20 +0200

    fixup! generate_makefile: fix command problems

--- generate_makefile.py
+++ generate_makefile.py
@@ -10,31 +10,31 @@ def userbool(prompt:str, default:bool=None) -> bool or default:
     accept_default = default is not None
     prompt += ' [{}/{}]'.format('Y' if default is True else 'y',
                                 'N' if default is False else 'n')
-    correct = lambda s: any(s.startswith(l) for l in 'yYnN') or (accept_default and s == '')
+    correct = lambda s: any(s.startswith(l) for l in 'yYnN') or (accept_default and s.strip() == '')
     answer = 'invalid'
     while not correct(answer):
         answer = input(prompt)
     return default if (accept_default and answer == '') else (answer.lower() == 'y')
 
 
-def userint(prompt:str, default:int=None) -> int:
+def userint(prompt:str, default:int=None) -> int or default:
     accept_default = default is not None
     prompt += ' [{}]'.format(default) if accept_default else ''
-    correct = lambda s: answer.isnumeric() or (accept_default and s == '')
+    correct = lambda s: answer.isnumeric() or (accept_default and s.strip() == '')
     answer = 'invalid'
     while not correct(answer):
         answer = input(prompt)
     return default if accept_default and not answer else int(answer)
 
 
-def userstring(prompt, default:str=None) -> str:
+def userstring(prompt, default:str=None) -> str or default:
     accept_default = default is not None
     prompt += ' [{}]'.format(default) if accept_default else ''
     answer = input(prompt)
     return default if accept_default and not answer else answer
 
 
-def user_choose_makefile(names:iter=('Makefile', 'makefile', 'makefile.f')) -> str:
+def user_choose_makefile(names:iter=('makefile', 'Makefile', 'makefile.f')) -> str:
     makefile = None
     for name in names:
         if os.path.exists(name):
@@ -49,42 +49,44 @@ def user_choose_makefile(names:iter=('Makefile', 'makefile', 'makefile.f')) -> s
 
 if __name__ == "__main__":
     recipes = OrderedDict()  # {recipe name: {commands}}
+    constants = []  # iterable of constants printed at the Makefile beginning
 
     # serve
-    command = 'uwsgi --socket 127.0.0.1:{} --wsgi-file shaarpli/shaarpli.py'
-    recipes['serve'] = {command.format(
-        userstring('UWSGI port', default=3031),
-    )}
+    constants += [
+        'UWSGI_URL=' + userstring('UWSGI server url ?', default='127.0.0.1'),
+        'UWSGI_PORT=' + str(userint('UWSGI port ?', default='3031')),
+    ]
+    recipes['serve'] = (
+        'uwsgi --socket $(UWSGI_URL):$(UWSGI_PORT) --wsgi-file shaarpli/shaarpli.py',
+    )
 
     # ssh
     if userbool('upload through ssh ?', default=True):
-        upload_command = 'scp -r -P {port} ./$OUTPUT_FILES {user}@{host}:{path}'
-        retrieve_command = 'scp -r -P {port} {user}@{host}:{path}/$OUTPUT_FILES ./'
-                    #mv shaarpli/ old_shaarpli/
-                        scp -r -P 9834 lucas@163.172.222.20:~/dev_www/shaarpli/{shaarpli.py,shaarpli,data} ./
+        upload_command = 'scp -r -P $(PORT) ./$(FILES) $(USER)@$(HOST):$(REMOTE_PATH)'
+        retrieve_command = 'scp -r -P $(PORT) $(USER)@$(HOST):$(REMOTE_PATH)/$(FILES) ./'
 
-        params = {
-            'host': userstring('hostname (IP or domain)', default='127.0.0.1'),
-            'port': userint('port', default=22),
-            'user': userstring('user', default=getpass.getuser()),
-            'path': userstring('path', default='~/shaarpli'),
-        }
-        recipes['upload'] = {upload_command.format(**params)}
-        recipes['retrieve'] = {
+        recipes['upload'] = (
+            upload_command,
+        )
+        recipes['retrieve'] = (
             'mkdir -p old_shaarpli/',
-            'mv $OUTPUT_FILES old_shaarpli/',
-            retrieve_command.format(**params),
-        }
-        constants = {
-            'OUTPUT_FILES={{shaarpli.py,shaarpli}}'
-        }
+            '- mv $(OUTPUT_FILES) old_shaarpli/',
+            retrieve_command,
+        )
+        constants += [
+            'FILES={shaarpli.py,shaarpli}',
+            'HOST=' + userstring('hostname (IP or domain)', default='127.0.0.1'),
+            'PORT=' + str(userint('port', default=22)),
+            'USER=' + userstring('user', default=getpass.getuser()),
+            'REMOTE_PATH=' + userstring('path', default='~/shaarpli'),
+        ]
 
 
     # write the Makefile
     makefile = user_choose_makefile()
     if makefile:
         with open(makefile, 'w') as fd:
-            fd.write('\n'.join(constants))
+            fd.write('\n'.join(constants) + '\n\n')
             for recipe, commands in recipes.items():
                 fd.write('{}:\n'.format(recipe))
                 fd.write('\t' + '\n\t'.join(commands) + '\n')
