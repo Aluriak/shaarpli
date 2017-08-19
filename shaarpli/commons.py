8931fd81c93148d622e2e539d27b1839006a5432
Aluriak <lucas.bourneuf@laposte.net>
2017-08-19 23:18:43 +0200

    commons: definition of Link object and file_content function

---
+++ shaarpli/commons.py
@@ -0,0 +1,20 @@
+"""Various definitions to be used accross the codebase.
+
+"""
+
+
+from collections import namedtuple
+
+
+Link = namedtuple('Link', 'title, description, link, publication_date')
+
+
+def file_content(filename:str, onfail='') -> str:
+    """Return the content of given filename, or onfail
+    if filename is not found."""
+    try:
+        with open(filename) as fd:
+            content = fd.read()
+    except FileNotFoundError:
+        content = onfail
+    return content
