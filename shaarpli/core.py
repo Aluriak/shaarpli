408bc82cfd7c63964a3c6afedf6b6de0859af4a9
Aluriak <lucas.bourneuf@laposte.net>
2017-08-20 23:50:15 +0200

    core: new caching system ; works better, and is simpler
    Use cachetools to implement a partial lru_cache over a function.
    The new cache system however lacks seriously of some features
    that was present in functools.lru_cache, including statistics
    and cache clearing.

--- shaarpli/core.py
+++ shaarpli/core.py
@@ -10,7 +10,10 @@ from itertools import islice
 from functools import lru_cache
 from urllib.parse import urlparse
 
+import cachetools
+
 from shaarpli import data
+from shaarpli import cache as cache_module
 from shaarpli import config as config_module
 from shaarpli import template
 from shaarpli.commons import Link
@@ -23,65 +26,78 @@ def page_for(env) -> str:
     """API entry point. Wait for CGI environnement"""
     # parse env and get static data
     config, cached_page_generator, db = load_static()
-    parameter = uri_parameter(env)
+    parameters = uri_parameters(env['REQUEST_URI'])
+    parameter = parameters[0] if len(parameters) > 0 else '1'
 
     # cache status request
     if parameter == 'cache':
-        return str(cached_page_generator.cache_info())
+        return cached_page_generator.cache.html_repr()
+
+    # At this point, parameters are invalid: replace them with default.
+    parameters, parameter = (), ''
+
+    # create default data if none available
+    if db.empty():
+        data.create_default_database(db.name)
+
+    # move the next link if needed
+    db.move_entry_if_expected()
 
     # other cases: show links
-    page_number = 1
-    if parameter.isnumeric():
-        page_number = max(1, int(parameter))
-    return generated_page(page_number, config, cached_page_generator, db)
+    page_number = max(1, int(parameter) if parameter.isnumeric() else 1)
 
-
-def generated_page(page_number:int, config, cached_page_generator, db) -> str:
+    # cache invalidation if data changed
     if db.out_of_date():
         cached_page_generator.cache_clear()
-    nb_link = int(db.nb_link or config.html.link_per_page)
-    if nb_link // int(config.html.link_per_page) >= page_number - 1:
-        return cached_page_generator(page_number, config, db)
-    else:  # not enough links to be readed
-        return redirection(config)
+
+    # return the requested page
+    return cached_page_generator(page_number, config, db)
+
+
+def page_generator(page_number:int, config, db) -> str:
+    """Return the html content of the page of given number.
+
+    page_number -- integer >= 1 giving the page requested by client
+    config  -- namedtuple like object giving configuration
+    db -- the database of links
+
+    """
+    nb_link_per_page = int(config.html.link_per_page)
+    page_of_link = lambda n: (n // nb_link_per_page) + 1  # link idx -> page number
+    links = iter(db.links)
+    next_links = lambda: islice(links, 0, nb_link_per_page)
+
+    # explore the links before those in requested page
+    page = 0  # fallback if page_number is 1
+    for page in range(1, page_number):
+        nb_link = sum(1 for link in next_links())
+        # redirect user if not enough links to continue
+        if nb_link < nb_link_per_page:
+            return redirection(config)
+
+    # now the *nb_link_per_page* next links must be rendered in the requested page
+    assert page == page_number - 1, "the last treated page is not just before the requested one"
+    return template.render_full_page(config, page_number, next_links())
 
 
 def redirection(config) -> str:
     return REDIRECTION.format(config.server.url)
 
 
-def page_generator(page_number:int, config:dict, db:data.DatabaseHandler) -> str:
-    """Return the page after templating.
-
-    page_number -- integer >= 1 giving the page requested by client
-    config  -- namedtuple like object giving configuration
-    db -- a data.DatabaseHandler instance
-
-    """
-    link_per_page = int(config.html.link_per_page)
-    if db.empty():
-        data.create_default_database(db.name)
-    start = (page_number-1) * link_per_page
-    db_reader = islice(db.links, start, start + link_per_page)
-    return template.render_full_page(config, page_number, db_reader)
-
-
-def uri_parameter(env) -> str:
+def uri_parameters(uri) -> str:
     """Return parameter in URI
 
-    >>> uri_parameter('/links')
-    ''
-    >>> uri_parameter('/links/')
-    ''
-    >>> uri_parameter('/links/4')
-    '4'
+    >>> uri_parameters('/links')
+    ()
+    >>> uri_parameters('/links/')
+    ()
+    >>> uri_parameters('/links/4')
+    ('4',)
+    >>> uri_parameters('/links/page/4')
+    ('page', '4')
 
     """
-    url = env['REQUEST_URI']
-    if '/' in url.lstrip('/'):
-        return url[url.rfind('/')+1:]
-    else:
-        return ''
+    return tuple(uri.strip('/').split('/')[1:])
 
 
 @lru_cache(maxsize=1)
@@ -90,7 +106,7 @@ def load_static() -> (tuple, callable, data.DatabaseHandler):
 
     Returns:
         config -- a config namedtuple (see config.py)
-        page_generator -- a wrapper around the `page` function
+        page_generator -- a wrapper around the `page_generator` function
         db -- a data.DatabaseHandler instance
 
     Without its lru_cache, this function can't assure the caching
@@ -105,9 +121,17 @@ def load_static() -> (tuple, callable, data.DatabaseHandler):
 
     # caching
     if cfg.server.cache_size and int(cfg.server.cache_size) > 0:
-        wrapper = lru_cache(maxsize=int(cfg.server.cache_size))
+        # this cache only consider the page number among
+        #  the parameters of page_generator
+        wrapper_key = lambda page, config, db: cachetools.keys.hashkey(page)
+        cache = cache_module.SLFUCache(int(cfg.server.cache_size))
+        wrapper = cachetools.cached(cache=cache, key=wrapper_key)
+                                    # maxsize=int(cfg.server.cache_size))
+
         cached_page_generator = wrapper(page_generator)
+        cached_page_generator.cache = cache
     else:
         cached_page_generator = page_generator
+        cached_page_generator.cache = None
 
     return cfg, cached_page_generator, db
