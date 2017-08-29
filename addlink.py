00ff0e7291a6f6a637ac2ab9eb7e68f2c8014372
Aluriak <lucas.bourneuf@laposte.net>
2017-08-29 17:23:50 +0200

    quick script used to add links into topublish database

---
+++ addlink.py
@@ -0,0 +1,33 @@
+
+import os
+import sys
+import csv
+import time
+from shaarpli.data import CSV_PARAMS
+from shaarpli import config
+
+CONFIG = config.get()
+DATABASE = CONFIG.autopublish.filepath
+try:
+    DATA_TO_ADD = sys.argv[1]
+except IndexError:
+    print('Expect first arg to be path to the file containing the information to add to database.')
+    exit(1)
+
+# extract data
+with open(DATA_TO_ADD) as fd:
+    title = next(fd).strip()
+    url = next(fd).strip()
+    body = fd.read().strip()
+    pubdate = int(time.time())
+
+
+# write data into database
+WORKING_DIR = os.dirname(os.realpath(sys.argv[1]))
+os.chdir(WORKING_DIR)
+
+with open(DATABASE, 'a') as fd:
+    writer = csv.writer(fd, **CSV_PARAMS)
+    writer.writerow((title, body, url, pubdate))
+
+print('DONE')
